1c1e9414c345bb7377201c7cd8c9ea07848dd72b
elkind <elkind@evcforum.net>
2023-04-14 11:29:35 -0400

    First attempt to talk to backend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import React, {useState, useEffect} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const useState(BackendInfo, backendInfoSet) = useState([])
+
+  useEffect(() => {
+    fetch('backend')
+      .then(res => res.json())
+      .then(BackendInfo)
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Here's the backend info: {BackendInfo}
         </p>
         <a
           className="App-link"
